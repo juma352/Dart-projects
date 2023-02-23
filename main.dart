5e7b8c860a47c7efbd5609f2857cac631eb90059
juma352 <84863026+juma352@users.noreply.github.com>
2023-02-23 19:09:08 +0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,34 @@
+void main () {
+  print('this is my first dart code');
+   String name = 'Alice';
+  int age = 30;
+  double height = 1.75;
+  bool isStudent = true;
+  
+  print('$name is $age years old and is $height meters tall. Is she a student? $isStudent');
+  var multiply = (int x, int y) {
+  return x * y;
+};
+print(multiply(2, 3));
+print(multiply(100, 2000));
+var divide = (int x, int y) {
+  return x / y;
+};
+print(divide(20, 4));
+void printGreeting(String name) {
+  print('Hello $name!');
+}
+printGreeting('Alice');
+
+  
+  }
+
+ 
+
+
+  
+
+
+
+
+  
